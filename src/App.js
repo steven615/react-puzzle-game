6e21245218919133cae08c606246f0d6c9a036fb
steven615 <stevenwang615@gmail.com>
2021-04-30 23:19:58 +1000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="board">
+        <div className="header">Sew the buttons on the jacket</div>
+        <div className="jacket"></div>
+
+        <div className="button-slot slot1"></div>
+        <div className="button-slot slot2"></div>
+        <div className="button-slot slot3"></div>
+        <div className="button-slot slot4"></div>
+        <div className="button-slot slot5"></div>
+
+        <div className="button-plate draggable plate1">
+          <div className="button-cont">
+            <div className="button-img"></div>
+            <div className="button-sewed"></div>
+            <div className="button-green"></div>
+          </div>
+        </div>
+        <div className="button-plate draggable plate2">
+          <div className="button-cont">
+            <div className="button-img"></div>
+            <div className="button-sewed"></div>
+            <div className="button-green"></div>
+          </div>
+        </div>
+        <div className="button-plate draggable plate3">
+          <div className="button-cont">
+            <div className="button-img"></div>
+            <div className="button-sewed"></div>
+            <div className="button-green"></div>
+          </div>
+        </div>
+        <div className="button-plate draggable plate4">
+          <div className="button-cont">
+            <div className="button-img"></div>
+            <div className="button-sewed"></div>
+            <div className="button-green"></div>
+          </div>
+        </div>
+        <div className="button-plate draggable plate5">
+          <div className="button-cont">
+            <div className="button-img"></div>
+            <div className="button-sewed"></div>
+            <div className="button-green"></div>
+          </div>
+        </div>
+        <div className="button-plate draggable plate6">
+          <div className="button-cont">
+            <div className="button-img"></div>
+            <div className="button-sewed"></div>
+            <div className="button-green"></div>
+          </div>
+        </div>
+        <div className="button-plate draggable plate7">
+          <div className="button-cont">
+            <div className="button-img"></div>
+            <div className="button-sewed"></div>
+            <div className="button-green"></div>
+          </div>
+        </div>
+        <div className="button-plate draggable plate8">
+          <div className="button-cont">
+            <div className="button-img"></div>
+            <div className="button-sewed"></div>
+            <div className="button-green"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
