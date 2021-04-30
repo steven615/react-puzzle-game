2e237d3aafe7735202725bf073d0e5c1259b7f27
steven615 <stevenwang615@gmail.com>
2021-05-01 00:22:15 +1000

    Add draggable

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+import React from 'react';
+import Draggable from 'react-draggable';
+
 import './App.scss';
 
-function App() {
+const App = () => {
+  const handleStart = (e) => {
+    // Change the cursor to grabbing
+    e.target.closest('.button-plate').style.cursor = 'grabbing';
+  }
+  const handleDrag = (e) => {
+  };
+
+  const handleStop = (e) => {
+    const plateBtn = e.target.closest('.button-plate');
+    // Change the cursor to grab
+    plateBtn.style.cursor = 'grab';
+  };
+
   return (
     <div className="App">
       <div className="board">
@@ -13,65 +29,105 @@ function App() {
         <div className="button-slot slot4"></div>
         <div className="button-slot slot5"></div>
 
-        <div className="button-plate draggable plate1">
-          <div className="button-cont">
-            <div className="button-img"></div>
-            <div className="button-sewed"></div>
-            <div className="button-green"></div>
+        <Draggable
+          onStart={handleStart}
+          onDrag={handleDrag}
+          onStop={handleStop}>
+          <div className="button-plate plate1">
+            <div className="button-cont">
+              <div className="button-img"></div>
+              <div className="button-sewed"></div>
+              <div className="button-green"></div>
+            </div>
           </div>
-        </div>
-        <div className="button-plate draggable plate2">
-          <div className="button-cont">
-            <div className="button-img"></div>
-            <div className="button-sewed"></div>
-            <div className="button-green"></div>
+        </Draggable>
+        <Draggable
+          onStart={handleStart}
+          onDrag={handleDrag}
+          onStop={handleStop}>
+          <div className="button-plate plate2">
+            <div className="button-cont">
+              <div className="button-img"></div>
+              <div className="button-sewed"></div>
+              <div className="button-green"></div>
+            </div>
           </div>
-        </div>
-        <div className="button-plate draggable plate3">
-          <div className="button-cont">
-            <div className="button-img"></div>
-            <div className="button-sewed"></div>
-            <div className="button-green"></div>
+        </Draggable>
+        <Draggable
+          onStart={handleStart}
+          onDrag={handleDrag}
+          onStop={handleStop}>
+          <div className="button-plate plate3">
+            <div className="button-cont">
+              <div className="button-img"></div>
+              <div className="button-sewed"></div>
+              <div className="button-green"></div>
+            </div>
           </div>
-        </div>
-        <div className="button-plate draggable plate4">
-          <div className="button-cont">
-            <div className="button-img"></div>
-            <div className="button-sewed"></div>
-            <div className="button-green"></div>
+        </Draggable>
+        <Draggable
+          onStart={handleStart}
+          onDrag={handleDrag}
+          onStop={handleStop}>
+          <div className="button-plate plate4">
+            <div className="button-cont">
+              <div className="button-img"></div>
+              <div className="button-sewed"></div>
+              <div className="button-green"></div>
+            </div>
           </div>
-        </div>
-        <div className="button-plate draggable plate5">
-          <div className="button-cont">
-            <div className="button-img"></div>
-            <div className="button-sewed"></div>
-            <div className="button-green"></div>
+        </Draggable>
+        <Draggable
+          onStart={handleStart}
+          onDrag={handleDrag}
+          onStop={handleStop}>
+          <div className="button-plate plate5">
+            <div className="button-cont">
+              <div className="button-img"></div>
+              <div className="button-sewed"></div>
+              <div className="button-green"></div>
+            </div>
           </div>
-        </div>
-        <div className="button-plate draggable plate6">
-          <div className="button-cont">
-            <div className="button-img"></div>
-            <div className="button-sewed"></div>
-            <div className="button-green"></div>
+        </Draggable>
+        <Draggable
+          onStart={handleStart}
+          onDrag={handleDrag}
+          onStop={handleStop}>
+          <div className="button-plate plate6">
+            <div className="button-cont">
+              <div className="button-img"></div>
+              <div className="button-sewed"></div>
+              <div className="button-green"></div>
+            </div>
           </div>
-        </div>
-        <div className="button-plate draggable plate7">
-          <div className="button-cont">
-            <div className="button-img"></div>
-            <div className="button-sewed"></div>
-            <div className="button-green"></div>
+        </Draggable>
+        <Draggable
+          onStart={handleStart}
+          onDrag={handleDrag}
+          onStop={handleStop}>
+          <div className="button-plate plate7">
+            <div className="button-cont">
+              <div className="button-img"></div>
+              <div className="button-sewed"></div>
+              <div className="button-green"></div>
+            </div>
           </div>
-        </div>
-        <div className="button-plate draggable plate8">
-          <div className="button-cont">
-            <div className="button-img"></div>
-            <div className="button-sewed"></div>
-            <div className="button-green"></div>
+        </Draggable>
+        <Draggable
+          onStart={handleStart}
+          onDrag={handleDrag}
+          onStop={handleStop}>
+          <div className="button-plate plate8">
+            <div className="button-cont">
+              <div className="button-img"></div>
+              <div className="button-sewed"></div>
+              <div className="button-green"></div>
+            </div>
           </div>
-        </div>
+        </Draggable>
       </div>
-    </div>
+    </div >
   );
-}
+};
 
 export default App;
